5c1be992ef47b0af10364b7cff0b32c450cc7041
Philipp Katz <philipp@philippkatz.de>
2022-01-02 17:50:48 +0100

    Update for v3.0.3

--- Formula/partial-emlx-converter.rb
+++ Formula/partial-emlx-converter.rb
@@ -3,8 +3,8 @@ require "language/node"
 class PartialEmlxConverter < Formula
   desc "Convert .emlx and .partial.emlx files created by Apple’s Mail.app to .eml"
   homepage "https://github.com/qqilihq/partial-emlx-converter#readme"
-  url "https://registry.npmjs.org/partial-emlx-converter/-/partial-emlx-converter-3.0.2.tgz"
-  sha256 "1901218bbb96b42e710c31e8524ca746354cd0c940639d18bc1aeab3beff0c0f"
+  url "https://registry.npmjs.org/partial-emlx-converter/-/partial-emlx-converter-3.0.3.tgz"
+  sha256 "ffb533b83bd8657e989b687a8694dfceee298de6d2cfcb6f51a233564c0d3c1d"
   license "MIT"
 
   livecheck do
